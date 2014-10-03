b9874de2350da894395bfd63bb24071d175e08eb
imacadmin <liuchenyi77@gmail.com>
2014-10-03 16:01:25 -0700

    Database update Nutrients update

--- app/src/main/java/uvic/csc/chenyil/consumptiontracker/database/MySQLiteHelper.java
+++ app/src/main/java/uvic/csc/chenyil/consumptiontracker/database/MySQLiteHelper.java
@@ -17,12 +17,23 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
     public static final String COLUMN_HEIGHT= "height";
     public static final String COLUMN_WEIGHT= "weight";
 
+    public static final String TABLE_NUTRIENTS = "nutrients";
+    public static final String COLUMN_UNIT = "unit";
 
-    private static final String DATABASE_NAME = "profiles.db";
-    private static final int DATABASE_VERSION = 2;
+    public static final String TABLE_LIMITSGOALS = "limitsgoals";
+    public static final String COLUMN_NID = "nid";
+    public static final String COLUMN_NVALUE= "NutrientValue";
+    public static final String COLUMN_TYPE="type";
+    public static final String COLUMN_PID="pid";
+
+//    private static final String DATABASE_NAME = "profiles.db";
+//    private static final int DATABASE_VERSION = 2;
+
+    private static final String DATABASE_NAME = "appdata.db";
+    private static final int DATABASE_VERSION = 1;
 
     // Database creation sql statement
-    private static final String DATABASE_CREATE = "create table "
+    private static final String PROFILES_CREATE = "create table "
             + TABLE_PROFILES + "("
             + COLUMN_ID + " integer primary key autoincrement, "
             + COLUMN_NAME + " text not null,"
@@ -31,19 +42,37 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
             + COLUMN_WEIGHT + " integer not null"
             + ");";
 
+    private static final String NUTRIENTS_CREATE = "create table "
+            + TABLE_NUTRIENTS + "("
+            + COLUMN_ID + " integer primary key autoincrement, "
+            + COLUMN_NAME + " text not null,"
+            + COLUMN_UNIT + "text not null,"
+            + ");";
+
+    private static final String LIMITSGOALS_CREATE = "create table "
+            + TABLE_LIMITSGOALS + "("
+            + COLUMN_ID + " integer primary key autoincrement, "
+            + COLUMN_NID + " REFERENCES "+TABLE_NUTRIENTS+"("+ COLUMN_ID +"),"
+            + COLUMN_NVALUE + " integer not null,"
+            + COLUMN_TYPE + " text not null,"
+            + COLUMN_PID + " FOREIGN KEY("+COLUMN_PID+") REFERENCES "+TABLE_PROFILES+"("+ COLUMN_ID +"),"
+            + ");";
+
     public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase database) {
-        database.execSQL(DATABASE_CREATE);
+        database.execSQL(PROFILES_CREATE);
+        database.execSQL(NUTRIENTS_CREATE);
+        database.execSQL(LIMITSGOALS_CREATE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         Log.w(MySQLiteHelper.class.getName(),
-                "Upgrading database from version " + oldVersion + " to "
+                "Upgrading database from version " + oldVersion  + " to "
                         + newVersion + ", which will destroy all old data");
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_PROFILES);
         onCreate(db);
